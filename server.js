fb7bff70aa13967c54e12ed524a5793dc198e38d
Ayush Ashish <ayushashish0911@gmail.com>
2021-06-03 09:15:57 +0530

    added controllers

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`)
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
